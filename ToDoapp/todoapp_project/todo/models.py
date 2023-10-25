e157cfb769fd222d009ed7a250d8baac61963636
unknown <congnang1242000@gmail.com>
2023-10-25 18:25:16 +0700

    update name

--- ToDoapp/todoapp_project/todo/models.py
+++ ToDoapp/todoapp_project/todo/models.py
@@ -1,3 +1,4 @@
+from datetime import timezone
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,8 +7,15 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     title = models.CharField(max_length= 255)
     description = models.TextField(null= True, blank= True)
+
     completed = models.BooleanField(default= False)
+
+    # status_complete = models.TextChoices("Not Yet", "Started", "Finished")
+    # completed = models.CharField(blank= True, choices= status_complete.choices)
+
     created_at = models.DateTimeField(auto_now_add= True)
+    # start_at = models.DateTimeField(default= timezone.now)
+    # finish_at = models.DateTimeField(null = True, blank= True)
     user = models.ForeignKey(User, on_delete= models.CASCADE, null= True, blank= True )
 
     def __str__(self):
